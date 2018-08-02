93c171a8397bc46847098ba83105c5f0672536e5
JohnMark <johnmarkludwick@gmail.com>
2018-08-02 02:38:03 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,19 @@
 class Dog
   
   attr_accessor :name, :breed
-  attr_reader :id
+  # attr_reader :id
   
-  def initialize(name, breed, id=nil)
-    @name = name
-    @breed = breed
-    @id = id
+  # def initialize(name, breed, id=nil)
+  #   @name = name
+  #   @breed = breed
+  #   @id = id
+  # end
+  
+  attr_accessor :name, :breed
+
+  def initialize(params = {})
+    @name = params.fetch(:name, ?)
+    @breed = params.fetch(:breed, ?)
   end
   
   
