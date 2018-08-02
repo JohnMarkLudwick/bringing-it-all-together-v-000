d65acc47b92632eacdc134722723b1d20792d036
JohnMark <johnmarkludwick@gmail.com>
2018-08-02 02:48:19 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -36,13 +36,22 @@ class Dog
     DB[:conn].execute(sql) 
   end
   
-  
-  
-  def self.create(name, breed)
-    student = Student.new(name, breed)
-    student.save
-    student
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade) 
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end 
   end
+  
+  
+  # def self.create(name, breed)
+  #   student = Student.new(name, breed)
+  #   student.save
+  #   student
+  # end
   
   
 end
