36ee50c995efd100bb00d26ddf37bb279db806c8
JohnMark <johnmarkludwick@gmail.com>
2018-08-02 02:50:01 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -36,22 +36,7 @@ class Dog
     DB[:conn].execute(sql) 
   end
   
-  def save
-    sql = <<-SQL
-      INSERT INTO dogs (name, breed) 
-      VALUES (?, ?)
-    SQL
-    DB[:conn].execute(sql, self.name, self.breed)
   
-    end 
-  end
-  
-  
-  # def self.create(name, breed)
-  #   student = Student.new(name, breed)
-  #   student.save
-  #   student
-  # end
   
   
 end
