f8715934ad30d7521d81a81645c627127d5a8f10
JohnMark <johnmarkludwick@gmail.com>
2018-08-02 02:41:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,7 +29,12 @@ class Dog
     DB[:conn].execute(sql) 
   end
   
-  
+  def self.drop_table
+    sql =  <<-SQL 
+      DROP TABLE dogs;
+        SQL
+    DB[:conn].execute(sql) 
+  end
   
   
   
